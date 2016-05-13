cd4c502efc0356c73fb10e677e8a14a8c34c0979
NatalaFireAir <natalya.fire@gmail.com>
2016-05-13 14:02:10 +0600

    with comments 1

--- src/com/Nikita/BinaryTreeDemo.java
+++ src/com/Nikita/BinaryTreeDemo.java
@@ -3,42 +3,36 @@ package com.Nikita;
 import java.util.Random;
 
 /**
- * Created by Natalie on 17.04.2016.
+ * Класс, демонстрирующий структуру данных - бинарное дерево, и основные операции с данной структурой
  */
 public class BinaryTreeDemo {
-    public static boolean checkSearchTree(Vertex vertex) {
-        if (vertex != null && (vertex.left != null && (vertex.key <= vertex.left.key || (checkSearchTree(vertex.left) != true))
-                || (vertex.right != null && (vertex.key >= vertex.right.key || checkSearchTree(vertex.right) != true)))) {
-            return false;
-        }
-        return true;
-    }
     public static void main(String args[]){
         Random rnd = new Random(System.currentTimeMillis());
         Vertex[] vertex = new Vertex[6];
-
+        /**
+         * Инициализация вершин случайными числами
+         */
         for (int i = 0; i < 6; i++) {
             vertex[i] = new Vertex(rnd.nextInt(20));
             System.out.print(vertex[i].key + " ");
-
         }
         System.out.println();
-
-        Vertex root;
-
+        /**
+         * Построение дерева
+         */
+        Vertex root = vertex[0];
         vertex[0].right = vertex[1];
         vertex[1].left = vertex[2];
         vertex[1].right = vertex[3];
         vertex[3].left = vertex[4];
         vertex[3].right = vertex[5];
-
-        root = vertex[0];
-
+        /**
+         * Чтение дерева слева направо и вычисление его характеристик: высоты, средней высоты, контрольной суммы
+         */
         Vertex.readLeftToRight(root);
         System.out.println();
         System.out.println("Height = " + Vertex.findHeight(root));
         System.out.println("Average height = " + Vertex.findAverageHeight(root));
         System.out.println("Check Sum = " + Vertex.findCheckSum(root));
-        System.out.println("Check = " + checkSearchTree(root));
     }
 }
--- src/com/Nikita/Vertex.java
+++ src/com/Nikita/Vertex.java
@@ -1,27 +1,25 @@
 package com.Nikita;
-
+/**
+ * Класс - Вершина дерева, описывает структуру вершины бинарного дерева.
+ * Каждую вершину можно рассматривать как отдельное дерево.
+ * Содержит целочисленное значение и ссылки на левого и правого потомка.
+ * */
 public class Vertex {
-    int data;
     int key;
     Vertex left;
     Vertex right;
+
     private static int max(int a, int b){
         if (a >= b) return a;
         return b;
     }
-    Vertex(){
-        key = 0;
-    }
-    Vertex(int data){
+    
+    public Vertex(int data){
         this.key = data;
     }
-    public void addLeft(Vertex vertex) {
-        this.left = vertex;
-    }
-    public void addRight(Vertex vertex) {
-        this.right = vertex;
-    }
-
+    /**
+     * Чтение делева слева направо
+     */
     public static void readLeftToRight(Vertex vertex) {
         if (vertex != null) {
             readLeftToRight(vertex.left);
@@ -29,37 +27,45 @@ public class Vertex {
             readLeftToRight(vertex.right);
         }
     }
-
+    /**
+     * Вычисление высоты дерева
+     */
     public static int findHeight(Vertex vertex) {
         int h;
         if (vertex == null) h = 0;
         else h = 1 + max(findHeight(vertex.left), findHeight(vertex.right));
         return h;
     }
-
+    /**
+     * Вычисление количества вершин дерева
+     */
     public static int findSize(Vertex vertex) {
         int size = 0;
         if (vertex != null) size = 1 + findSize(vertex.left) + findSize(vertex.right);
         return size;
     }
-
+    /**
+     * Вычисление суммы длин путей до вершины
+     */
     public static int pathsLengthSum(Vertex vertex, int level) {
         int pathsSum;
         if (vertex == null)pathsSum = 0;
         else pathsSum = level + pathsLengthSum(vertex.left, level + 1) + pathsLengthSum(vertex.right, level + 1);
         return pathsSum;
     }
-
+    /**
+     * Вычисление средней высоты дерева
+     */
     public static float findAverageHeight(Vertex vertex) {
-    //    System.out.println(pathsLengthSum(vertex, 1));
         return (float)pathsLengthSum(vertex, 1)/findSize(vertex);
     }
-
+    /**
+     * Вычисление контрольной суммы дерева(суммы ключей всех вершин)
+     */
     public static int findCheckSum(Vertex vertex) {
         int checkSum;
         if (vertex == null) checkSum = 0;
         else checkSum = vertex.key + findCheckSum(vertex.left) + findCheckSum(vertex.right);
         return checkSum;
     }
-
 }
